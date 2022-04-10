de619ef2e4579af66461ed720c963aa5cf2338d1
Alex Soares <alex.caminhas.5120@gmail.com>
2022-04-10 00:42:17 -0300

    Starting to implement board and piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,23 @@
 package application;
 
+import java.util.Scanner;
+
 import boardgame.Position;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		Position position = new Position(3, 5);
+		Position position = new Position();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite o valor da linha: ");
+		position.setRowInteger(sc.nextInt());
+		System.out.print("Digite o valor da coluna: ");
+		position.setColumn(sc.nextInt());
+		
 		System.out.println(position);
+		
+		sc.close();
 		
 	}
 	
